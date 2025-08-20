dac52a1cbfa13cad8eab3750d9640ceb4c405b8b
gcnaveen <naveengc99@gmail.com>
2025-08-20 21:55:39 +0530

    video added

--- dist/assets/HeroSection-L9UH9IXr.js
+++ dist/assets/HeroSection-L9UH9IXr.js
@@ -1,4 +1,4 @@
-import{g as et,r as A,j as x,R as Ct,u as zt}from"./index-E4oj3jeG.js";var ve={exports:{}};/*!
+import{g as et,r as A,j as x,R as Ct,u as zt}from"./index-FRA1ikDH.js";var ve={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
--- src/pages/DonatePage.jsx
+++ src/pages/DonatePage.jsx
@@ -244,7 +244,7 @@ const DonatePage = () => {
               type="submit"
               className="w-full bg-blue-600 text-white font-bold py-3 rounded-md shadow hover:bg-blue-700 transition-colors duration-300"
             >
-              {isEnglish ? "Donate Now" : "ಇದೀಗ ದಾನಮಾಡಿ"}
+              {isEnglish ? "Donate Now" : "ದೇಣಿಗೆ ನೀಡಿ"}
             </button>
           </form>
         </div>
